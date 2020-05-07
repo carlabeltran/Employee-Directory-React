9033c4f00f00eee0f8b14d7468465c4bf08b2216
carlabeltran <carlabeltran14@icloud.com>
2020-05-06 23:38:43 -0500

    Updates to App.js
    Added code from activity 18 fun with forms example

--- src/App.js
+++ src/App.js
@@ -1,56 +1,8 @@
-//COMPONENT LIBRARY
-import React from 'react';
-//LOG
-import logo from './logo.svg';
-//CSS
-import './App.css';
-// import { render } from 'react-dom';
+import React from "react";
+import Form from "./components/Form";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          Hello!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Form />;
 }
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h1>Welcome to React</h1>
-//           <p>
-//             Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//           <a
-//             className="App-link"
-//             href="https://reactjs.org"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             Learn React
-//         </a>
-//         </header>
-//       </div>
-//     );
-//   }
-// }
-
-//EXPORT THE WHOLE THING
 export default App;
